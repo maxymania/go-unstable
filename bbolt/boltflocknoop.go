b357a87a14f21837c3da8ab00bdf23d10c38db73
Simon Schmidt <simon.schmidt@htp-tel.de>
2018-11-29 19:40:17 +0100

    make Windows compile

--- bbolt/boltflocknoop.go
+++ bbolt/boltflocknoop.go
@@ -1,4 +1,4 @@
-// +build !windows,!plan9,!solaris
+// +build windows
 
 /*
 Copyright (c) 2018 Simon Schmidt
@@ -27,43 +27,11 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 package bbolt
 
 import (
-	"syscall"
 	"time"
 )
 
 // flock acquires an advisory lock on a file descriptor.
-func flock(db *DB, exclusive bool, timeout time.Duration) error {
-	var t time.Time
-	if timeout != 0 {
-		t = time.Now()
-	}
-	fd := db.file.Fd()
-	flag := syscall.LOCK_NB
-	if exclusive {
-		flag |= syscall.LOCK_EX
-	} else {
-		flag |= syscall.LOCK_SH
-	}
-	for {
-		// Attempt to obtain an exclusive lock.
-		err := syscall.Flock(int(fd), flag)
-		if err == nil {
-			return nil
-		} else if err != syscall.EWOULDBLOCK {
-			return err
-		}
-
-		// If we timed out then return an error.
-		if timeout != 0 && time.Since(t) > timeout-flockRetryTimeout {
-			return ErrTimeout
-		}
-
-		// Wait for a bit and try again.
-		time.Sleep(flockRetryTimeout)
-	}
-}
+func flock(db *DB, exclusive bool, timeout time.Duration) error { return nil }
 
 // funlock releases an advisory lock on a file descriptor.
-func funlock(db *DB) error {
-	return syscall.Flock(int(db.file.Fd()), syscall.LOCK_UN)
-}
+func funlock(db *DB) error { return nil }
